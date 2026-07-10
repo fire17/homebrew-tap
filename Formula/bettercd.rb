d17000a8e965c0640756fad6845d4fa9b7ef736d
Tami <fire17@gmail.com>
2026-07-10 04:02:00 +0300

    bettercd 0.2.1

--- Formula/bettercd.rb
+++ Formula/bettercd.rb
@@ -1,8 +1,8 @@
 class Bettercd < Formula
   desc "Better cd - zoxide-aware, auto-mkdir, with undo. Pure shell, zero deps"
   homepage "https://github.com/fire17/bettercd"
-  url "https://github.com/fire17/bettercd/archive/refs/tags/v0.2.0.tar.gz"
-  sha256 "7e2ac939e2280098466f09daeb1446e88983de0cce04d0f0bf9dfc5f44a33ce6"
+  url "https://github.com/fire17/bettercd/archive/refs/tags/v0.2.1.tar.gz"
+  sha256 "52d6a4968f630f513b4379fcdf123f968c3040d117d3245659fd4599d66146e4"
   license "MIT"
 
   def install
